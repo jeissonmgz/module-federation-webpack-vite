c9ddc423901bc971e25419ea7553ef3018b2b6d6
jeissonmgz <jeissonmgz@gmail.com>
2023-02-28 22:02:38 -0500

    :tada: Initialize shell and mfe

---
+++ shell/config/webpack.dev.js
@@ -0,0 +1,28 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const packageJson = require('../package.json')
+
+const devConfig = {
+    mode: 'development',
+    output: {
+        publicPath: 'http://localhost:3000/'
+    },
+    devServer: {
+        port: 3000,
+        historyApiFallback: {
+            index: '/index.html'
+        }
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'container',
+            remotes: {
+                helloReact: 'helloReact@http://localhost:3010/remoteEntry.js'
+            },
+            shared: packageJson.dependencies
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig)
